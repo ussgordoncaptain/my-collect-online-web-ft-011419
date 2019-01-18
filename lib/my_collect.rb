f4883e387ce0c412851065625a27cc5c224dc77a
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-18 21:53:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   ii= 0 
   final = []
     while (ii<array.size)
-      final<<yield(array[ii])
-      ii -= 1
+      final << yield(array[ii])
+      ii += 1
     end
     return final
   end
